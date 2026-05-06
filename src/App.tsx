5c776acdce3de00af1b52d47cdb7155938badeb0
Mirelly <mirelly.est.adv@gmail.com>
2026-05-06 19:07:52 -0300

    implementação-navbar-e-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home"
 
 function App() {
   
  return (
- <Home/>
+    <>
+        <Navbar/>
+        <Home/>
+        <Footer/>
+ 
+   </>
  );
 }
 
